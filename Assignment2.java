499750f3d8b4beabbd3a91a8ab0a645bcf9f2578
Mellorine <92373116+WildNikoloz@users.noreply.github.com>
2025-03-20 11:47:29 +0400

    Assignment 20/03/2025

---
+++ Assignment2.java
@@ -0,0 +1,37 @@
+package workshop.w1.nikoloz_jintcharadze_2;
+
+/**
+ * Find two packages inside this package.
+ * Create classes A and B inside first and the second package accordingly
+ * 1 point
+*/
+
+public class iliaunirules {
+    public static void main(String[] args) {
+        int A = 7;
+    }
+}
+
+public class javarules {
+    public static void main(String[] args) {
+        int B = 13;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+package workshop.w1.nikoloz_jintcharadze_2;
+
+
+/**
+ * Find the file "hidden.txt" and print its path.
+ * The file should be found yourself without java code.
+ * 0.5 points
+ */
+public class Assignment03 {
+	public static void main(String[] args) {
+	    System.out.printf("workshop/w1/nikoloz_jintcharadze_2/webcam/highlighter/suv/hidden.txt");
+	}
+}
